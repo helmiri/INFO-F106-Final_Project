d4a14921ae235e89247c6b29fae86469d426e0f3
helmiri <Hamza.El.miri@ulb.be>
2019-03-30 15:48:46 +0100

    Added control parameters

---
+++ config.py
@@ -0,0 +1,280 @@
+'''
+    File name: config.py
+    Author: El Miri Hamza
+    ID: 000479603
+    Date: 03/03/2019
+
+    Function:
+        - Contains variables shared across partie3.py and main_partie2.py
+        - Creates a UI for the board
+'''
+
+from PyQt5.QtGui import QPen, QBrush, QColor
+from PyQt5.QtWidgets import QApplication, QProgressBar, QMainWindow, QGraphicsScene, QGraphicsView, QGraphicsRectItem, QGraphicsEllipseItem, QGraphicsTextItem
+from PyQt5.QtCore import QRectF, Qt
+
+
+class Parameter:
+    """
+    Class containing all of the parameters that will be used
+    """
+
+    def __init__(self):
+        # AI
+        self.size = 5
+        self.walls = 1
+        self.epsilon = 0.3
+        self.alpha = 0.4
+        self.lamb = 0.9
+        self.learning_strategy = 'Q-Learning'
+        self.g = None
+        self.g_init = None
+        self.neurons = 40
+        self.activation = "Sigmoid"
+        self.eps_decrease = 0
+
+        # Game
+        self.train = 10000
+        self.compare = 10000
+        self.NN = None
+        self.board = None
+
+        # Wether or not to update the board
+        self.training = True
+
+        # Control thingies
+        self.running = True
+        self.keyPressed = None
+        self.closed = False
+        self.endGame = False
+
+
+class Board(QMainWindow):
+    """
+    Board window using QGraphicsView and QGraphicsScene
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Make AI Great Again")
+        self.scene = QGraphicsScene()
+        self.view = QGraphicsView(self.scene)
+        self.setCentralWidget(self.view)
+
+        self.message = QGraphicsTextItem()
+        self.scene.addItem(self.message)
+
+        self.current_player = QGraphicsTextItem()
+        self.scene.addItem(self.current_player)
+
+        self.probablity = QGraphicsTextItem()
+        self.scene.addItem(self.probablity)
+
+        # Create player
+        self.player1 = QGraphicsEllipseItem(QRectF(0, 0, 90, 90))
+        self.player2 = QGraphicsEllipseItem(QRectF(0, 0, 90, 90))
+        self.player2.setBrush(Qt.black)
+        self.player1.setBrush(Qt.white)
+
+    def initBoard(self):
+        # Create board cells
+        self.item_list = list()
+        for i in range(parameter.size):
+            for j in range(parameter.size):
+                cell = QGraphicsRectItem(QRectF(j * 100, i * 100, 100, 100))
+                cell.setBrush(QBrush(Qt.gray))
+                self.item_list.append(cell)
+
+        for item in self.item_list:
+            self.scene.addItem(item)
+
+        # Create all of the walls that will be used
+        self.walls_on_board = {'Horizontal': [], 'Vertical': []}
+        self.walls_on_board['Horizontal'] = self.create_the_damn_walls(200, 5)
+        self.walls_on_board['Vertical'] = self.create_the_damn_walls(5, 200)
+
+        # Eeach player has these walls.
+        self.wallsPlayer1 = list()
+        self.wallsPlayer2 = list()
+        i = 0
+        while len(self.wallsPlayer1) < parameter.walls:
+            # Initialize to False to avoid this warning: QGraphicsScene::addItem: item has already been added to this scene
+            # In subsequent init calls with different wall numbers
+            self.wallsPlayer1.append([QGraphicsRectItem(QRectF(-50 - i * 30, (parameter.size - 2) * 100, 5, 200)), False])
+            self.wallsPlayer2.append([QGraphicsRectItem(QRectF((parameter.size * 100) + 50 + i * 30, 0, 5, 200)), False])
+
+            if not self.wallsPlayer1[i][1]:
+                self.scene.addItem(self.wallsPlayer1[i][0])
+                self.scene.addItem(self.wallsPlayer2[i][0])
+                self.wallsPlayer1[i][1] = True
+                self.wallsPlayer2[i][1] = True
+            i += 1
+
+        # Board messages
+        self.current_player.setPos(parameter.size * 100 + 25, 250)
+        self.message.setPos(parameter.size * 100 + 25, 300)
+        self.probablity.setPos(parameter.size * 100 + 25, 375)
+
+        self.scene.addItem(self.player1)
+        self.scene.addItem(self.player2)
+
+        self.showMaximized()
+
+    def create_the_damn_walls(self, width, height):
+        """
+        self.explanatory
+            - return: List containing QGraphicsRectItem for every wall that will be used (player1 + player2)
+        """
+        lst = list()
+        for i in range(parameter.walls * 2):
+            wall = QGraphicsRectItem(QRectF(0, 0, width, height))
+            # Red paint to symbolize my pain and suffering while implementing them
+            wall.setBrush(QBrush(Qt.red))
+            lst.append([wall, False])
+
+        return lst
+
+    def closeEvent(self, event):
+
+        # Delete items in QGraphicsScene otherwise reducing the size of the board won't work
+        for item in self.item_list:
+            self.scene.removeItem(item)
+
+        # Reset available walls
+        for i in range(parameter.walls):
+            if self.wallsPlayer1[i][1]:
+                self.scene.removeItem(self.wallsPlayer1[i][0])
+            if self.wallsPlayer2[i][1]:
+                self.scene.removeItem(self.wallsPlayer2[i][0])
+                self.wallsPlayer2[i][1] = True
+
+        for i in range(len(self.walls_on_board['Horizontal'])):
+            if self.walls_on_board['Horizontal'][i][1]:
+                self.scene.removeItem(self.walls_on_board['Horizontal'][i][0])
+            if self.walls_on_board['Vertical'][i][1]:
+                self.scene.removeItem(self.walls_on_board['Vertical'][i][0])
+
+        self.scene.removeItem(self.player1)
+        self.scene.removeItem(self.player2)
+
+        # Break from while loop in waitForKey()
+        parameter.keyPressed = Qt.Key_Q
+        parameter.running = False
+        parameter.closed = True
+
+    def keyPressEvent(self, event):
+
+        parameter.keyPressed = event.key()
+        # Break from waitForKey()
+        parameter.running = False
+
+    def display(self, lboard, msg=''):
+        """
+        Update board
+
+                args:
+                    - lboard: List containing the board state
+        """
+        white_player = lboard[0]
+        black_player = lboard[1]
+        horizontal_walls = lboard[2]
+        vertical_walls = lboard[3]
+        walls_left_p1 = lboard[4]
+        walls_left_p2 = lboard[5]
+
+        # Check diagonal jump
+        if white_player[0] == black_player[0] and white_player[1] == black_player[1]:
+            x = white_player[0] * 100 + 5 + 50
+            x2 = white_player[0] * 100 - 50
+            self.player1.setPos(x, (white_player[1] - (parameter.size - 1)) * -100 + 5)
+            self.player2.setPos(x2, (white_player[1] - (parameter.size - 1)) * -100 + 5)
+
+        else:
+            self.player2.setPos(black_player[0] * 100 + 5, (black_player[1] - (parameter.size - 1)) * -100 + 5)
+            self.player1.setPos(white_player[0] * 100 + 5, (white_player[1] - (parameter.size - 1)) * -100 + 5)
+
+        self.addWallsBoard(horizontal_walls, self.walls_on_board['Horizontal'], 'Horizontal')
+        self.addWallsBoard(vertical_walls, self.walls_on_board['Vertical'], 'Vertical')
+
+        self.removeWalls(walls_left_p1, self.wallsPlayer1)
+        self.removeWalls(walls_left_p2, self.wallsPlayer2)
+
+    def removeWalls(self, wallsLeft, player):
+        """
+        Removes or adds available walls
+
+                - args:
+                    - wallsLeft: Walls each player has available
+                    - player: Which player's walls to update
+        """
+        available_walls = 0
+        i = 0
+        found = False
+        while i < parameter.walls and not found:
+            if player[i][1]:
+                available_walls += 1
+            else:
+                found = True
+            i += 1
+
+        if wallsLeft < available_walls:
+            i = parameter.walls - 1
+            found = False
+            while i >= wallsLeft and not found:
+                if player[i][1]:
+                    self.scene.removeItem(player[i][0])
+                    player[i][1] = False
+                    found = True
+                i -= 1
+
+        elif wallsLeft > available_walls:
+            # this could have been rewritten as a while loop, and stop once it updates the wall
+            # in question (break) as it would've been more efficient. But, it wouldn't work in case
+            # the game ends as it would only reset 1 wall instead of all of them.
+            for i in range(parameter.walls):
+                if not player[i][1]:
+                    self.scene.addItem(player[i][0])
+                    player[i][1] = True
+                    if not parameter.endGame:
+                        break
+
+    def addWallsBoard(self, orientation_lst, walls_lst, orientation):
+        """
+        Calculates wall coordinates and updates their position on screen or removes the las placed wall
+        when user switches between horizontal-vertical placements
+
+            args:
+                - orientation_lst: List containing the coordinates of the walls to update
+                - walls_lst: List containing wall objects to place
+                - orientation: Wall's orientation
+        """
+        # Hallelujah! This is the most efficient and consistent way I found to work.
+        # Please forgive the usage of the 'forbidden instruction'
+        i = 0
+        while i < len(walls_lst):
+            try:
+                x = orientation_lst[i][0] * 100
+                y = ((orientation_lst[i][1] - (parameter.size - 1)) * -100)
+
+                # Offset depending on wall orientation
+                x += 100 - 5/2 if orientation == 'Vertical' else 0
+                y -= 5 / 2 if orientation == 'Horizontal' else 100
+
+                walls_lst[i][0].setPos(x, y)
+                if not walls_lst[i][1]:
+                    walls_lst[i][1] = True  # Wall has been placed
+                    self.scene.addItem(walls_lst[i][0])
+                i += 1
+            except IndexError:
+                # Lenght of orientation_lst < self.walls_on_board['X']: Wall placement switched between vertical/horizontal or cancelled
+                # Remove last placed wall from scene
+                if walls_lst[i][1]:
+                    walls_lst[i][1] = False
+                    self.scene.removeItem(walls_lst[i][0])
+                i += 1
+
+
+parameter = Parameter()
+mandatory_QApplication_creation = QApplication([])
+super_awesome_board = Board()  # I ran out of variable name ideas
+loading_bar = QProgressBar()
---
+++ partie4.py
@@ -0,0 +1,335 @@
+'''
+    File name: partie3.py
+    Author: El Miri Hamza
+    ID: 000479603
+    Date: 03/03/2019
+
+    Function:
+        UI for main_partie2.py
+'''
+
+import sys
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QLabel, QGridLayout, QComboBox, QAction, QStatusBar, QSpinBox, QDoubleSpinBox, QPushButton, QFileDialog, QProgressBar
+from config import parameter, super_awesome_board, loading_bar
+from main_partie4 import *
+
+
+class Main(QMainWindow):
+
+    def __init__(self):
+        super(Main, self).__init__()
+        self.title = "Quoridor"
+        self.x = 700
+        self.y = 300
+        self.width = 500
+        self.height = 550
+        self.initUI()
+
+    def initUI(self):
+        self.setWindowTitle('Quoridor: Deluxe Edition')
+        self.setFixedWidth(500)
+        self.setFixedHeight(550)
+
+        self.options = Options()
+
+        self.setCentralWidget(self.options)
+        self.show()
+
+        menuBar = self.menuBar()
+        file = menuBar.addMenu("File")
+        file.addAction("Load AI")
+        file.addAction("Save AI")
+        file.triggered[QAction].connect(self.fileMenu)
+
+        self.statusBar = QStatusBar()
+        self.setStatusBar(self.statusBar)
+
+        # To be used in Buttons class
+        self.loading_label = QLabel('')
+        self.loading_bar_added = False
+
+    def fileMenu(self, trigger):
+
+        if trigger.text() == "Load AI":
+            AI_load = QFileDialog.getOpenFileName(self, "Load AI", "", "Load AI file(*.npz)")
+            data = np.load(AI_load[0])
+            parameter.size = int(data['N'])
+            parameter.walls = int(data['WALLS'])
+            parameter.NN = (data['W1'], data['W2'])
+            parameter.g_init = computeGraph()
+            self.statusBar.showMessage(str(AI_load[0]) + '  Loaded', 5000)
+
+        if trigger.text() == "Save AI":
+            if parameter.NN is not None:
+                AI_save = QFileDialog.getSaveFileName(self, "Save AI", "", "Save AI File(*.npz")
+                np.savez(AI_save[0], N=parameter.size, WALLS=parameter.walls, W1=parameter.NN[0], W2=parameter.NN[1])
+                self.statusBar.showMessage(str(AI_save[0]) + '  Saved', 5000)
+            else:
+                self.statusBar.showMessage("You must first create or load an AI first!", 5000)
+
+
+class DropDownMenu(QComboBox):
+
+    def __init__(self, menu, items):
+        """
+            - args:
+                - menu: Menu text
+                - items: Items to be added
+        """
+        super().__init__()
+
+        for i in range(len(items)):
+            self.addItem(items[i])
+
+        self.menu = menu
+
+        if menu == 'Strategy':
+            self.currentTextChanged.connect(self.setStrategy)
+        else:
+            self.currentTextChanged.connect(self.setActivation)
+
+    def setStrategy(self, text):
+        parameter.strategy = self.currentText()
+    
+    def setActivation(self, text):
+        parameter.activation = self.currentText()
+
+
+class Options(QWidget):
+    """
+    Option widgets
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        self.grid = QGridLayout()
+
+        # Create items
+        self.strategy = DropDownMenu('Strategy', ['Q-Learning', 'TD-Lambda'])
+        self.activation = DropDownMenu('Activation', ['Sigmoid','ReLu', 'Swish'])
+
+        self.board_size = self.create_box('BoardSize', 'SpinBox', [3, 9], 1)
+        self.nbr_walls = self.create_box('Walls', 'SpinBox', [0, 10], 1)
+        self.nbr_games_train = self.create_box('Train', 'SpinBox', [1, 10000000], 500)
+        self.nbr_games_compare = self.create_box('Compare', 'SpinBox', [1, 10000000], 500)
+        self.nbr_neurons = self.create_box('Neurons', 'SpinBox', [0, 10000000], 1)
+        self.epsilon = self.create_box('Epsilon', 'DoubleSpinBox', [0.00, 1.00], 0.05)
+        self.alpha = self.create_box('Alpha', 'DoubleSpinBox', [0.00, 1.00], 0.05)
+        self.lambda_ = self.create_box('Lambda', 'DoubleSpinBox', [0.00, 1.00], 0.05)
+        self.eps_decrease = self.create_box('Decreasing Epsilon', 'DoubleSpinBox', [0, 1], 0.05)
+        
+
+        self.trainAI = Buttons('Train', 'Train AI')
+        self.compareAI = Buttons('Compare', 'Compare AI')
+        self.playAI = Buttons('PlayAI', 'Play AI')
+        self.playHuman = Buttons('PlayHuman', 'Play Human')
+
+        self.initWidgets()
+
+    def initWidgets(self):
+        # Place items in a grid
+
+        labels = ['Strategy:', 'Activation function','Neurons', 'Board Size:', 'Number of walls:',
+                  'Number of games to train the AI:', 'Number of games to compare the AI:',
+                  'Epsilon:', 'Epsilon decrease rate', 'Learning rate:', 'Lambda']
+
+        options = [self.strategy, self.activation, self.nbr_neurons, self.board_size,
+                   self.nbr_walls, self.nbr_games_train, self.nbr_games_compare, self.epsilon,
+                   self.eps_decrease, self.alpha, self.lambda_, self.trainAI, self.compareAI,
+                   self.playAI, self.playHuman]
+
+        for i in range(len(options)):
+            if i < len(labels):
+                self.grid.addWidget(QLabel(labels[i]), i, 0)
+                self.grid.addWidget(options[i], i, 1)
+            else:
+                if i % 2 == 1:
+                    self.grid.addWidget(options[i], i, 0)
+                else:
+                    self.grid.addWidget(options[i], i - 1, 1)
+
+        self.setLayout(self.grid)
+
+    def create_box(self, option, box_type, box_range, step):
+        """
+        Creates SpinBoxes
+
+                args:
+                    - option: String with the name of the option to be created
+                    - box_type: Either SpinBox or DoubleSpinBox
+                    - box_rage: Minimum and maximum values of the box
+                    - step: Increment size
+        """
+
+        if box_type == 'SpinBox':
+            box = QSpinBox()
+        else:
+            box = QDoubleSpinBox()
+
+        box.setSingleStep(step)
+        box.setMinimum(box_range[0])
+        box.setMaximum(box_range[1])
+
+        # Set values
+
+        if option == 'BoardSize':
+            box.valueChanged.connect(self.update_board_size)
+            box.setValue(parameter.size)
+        elif option == 'Walls':
+            box.valueChanged.connect(self.update_walls)
+            box.setValue(parameter.walls)
+        elif option == 'Epsilon':
+            box.valueChanged.connect(self.update_epsilon)
+            box.setValue(parameter.epsilon)
+        elif option == 'Alpha':
+            box.valueChanged.connect(self.update_alpha)
+            box.setValue(parameter.alpha)
+        elif option == 'Lambda':
+            box.valueChanged.connect(self.update_lambda)
+            box.setValue(parameter.lamb)
+        elif option == 'Neurons':
+            box.valueChanged.connect(self.update_neurons)
+            box.setValue(parameter.neurons)
+        elif option == 'Train':
+            box.valueChanged.connect(self.update_train)
+            box.setValue(parameter.train)
+        elif option == 'Compare':
+            box.valueChanged.connect(self.update_compare)
+            box.setValue(parameter.compare)
+        elif option == 'Decreasing Epsilon':
+            box.valueChanged.connect(self.update_decrease_eps)
+            box.setValue(parameter.eps_decrease)
+        return box
+
+    def update_decrease_eps(self, value):
+        parameter.eps_decrease = value
+        
+    def update_board_size(self, value):
+        parameter.size = value
+
+    def update_walls(self, value):
+        parameter.walls = value
+
+    def update_neurons(self, value):
+        parameter.neurons = value
+
+    def update_epsilon(self, value):
+        parameter.epsilon = value
+
+    def update_alpha(self, value):
+        parameter.alpha = value
+
+    def update_lambda(self, value):
+        parameter.lamb = value
+
+    def update_train(self, value):
+        parameter.train = value
+
+    def update_compare(self, value):
+        parameter.compare = value
+
+
+class Buttons(QPushButton):
+    def __init__(self, button, text):
+        """
+                - args:
+                    - button: Name of the button to be created
+                    - text: Button text
+        """
+        super().__init__()
+        self.setText(text)
+
+        if button == 'Train':
+            self.clicked.connect(self.trainAI)
+        elif button == 'Compare':
+            self.clicked.connect(self.compareAI)
+        elif button == 'PlayAI':
+            self.clicked.connect(self.playAI)
+        else:
+            self.clicked.connect(self.playHuman)
+
+
+    def trainAI(self):
+        parameter.training = True
+        parameter.board = startingBoard()
+        parameter.g_init = computeGraph()
+        parameter.g = parameter.g_init.copy()
+        parameter.NN = createNN(2 * parameter.size ** 2 + 2 * (parameter.size - 1) ** 2 + 2 * (parameter.walls + 1), parameter.neurons)
+
+        if not main.loading_bar_added:
+            main.statusBar.insertWidget(0, loading_bar, 20)
+            main.loading_bar_added = True
+
+        loading_bar.setValue(0)
+
+        # Remove previous Qlabel before adding the new one
+        main.statusBar.removeWidget(main.loading_label)
+        main.loading_label = QLabel("Training hard. Please wait...")
+        main.statusBar.insertWidget(0, main.loading_label, 10)
+        train(parameter.NN, parameter.train)
+        loading_bar.setValue(100)  # Because it stops at 99% >:c
+        main.statusBar.insertWidget(0, main.loading_label, 10)
+
+        # For some reason, showMessage doesn't obscure the widgets as stated  in the documentation
+        # Only way to do it is by main.statusBar.removeWidget(main.loading_bar) and then using showMessage
+        # to display it. But nothing is that simple! Because for some reason, once main.loading_bar is removed
+        # from statusBar, it can no longer be added. So, I'm using QLabels to get around the problem.
+
+        # main.statusBar.removeWidget(main.oading_bar)
+        # main.statusBar.showMessage("Ready to kick some butt!", 5000)
+        main.statusBar.removeWidget(main.loading_label)
+        main.loading_label = QLabel("Ready to kick some butt!")
+        main.statusBar.insertWidget(0, main.loading_label, 10)
+
+    def compareAI(self):
+        AI_load = QFileDialog.getOpenFileName(self, "Load AI", "", "Load AI file(*.npz)")
+        filename = AI_load[0]
+
+        if not main.loading_bar_added:
+            main.statusBar.insertWidget(0, loading_bar, 20)
+            main.loading_bar_added = True
+        loading_bar.setValue(0)
+
+        main.statusBar.removeWidget(main.loading_label)
+
+        main.loading_label = QLabel("{0} Games with Epsilon = {1}".format(parameter.compare, parameter.epsilon))
+        main.statusBar.insertWidget(0, main.loading_label, 10)
+        wins = compare(parameter.NN, filename, parameter.compare, parameter.epsilon)
+        loading_bar.setValue(100)
+
+        # main.statusBar.removeWidget(loading_bar)
+        # main.statusBar.showMessage("Games won by the AI:  " + wins, 5000)
+        main.statusBar.removeWidget(main.loading_label)
+        main.loading_label = QLabel("Games won by the AI:  " + wins + " ")
+        main.statusBar.insertWidget(0, main.loading_label, 10)
+
+    def playAI(self):
+        super_awesome_board.initBoard()
+        human = Player_Human('Humain')
+        agent = Player_AI(parameter.NN, 0.0, None, 'IA')
+        parameter.training = False
+        parameter.boardClosed = False
+        play(human, agent)
+
+    def playHuman(self):
+        parameter.training = False
+        parameter.boardClosed = False
+
+        super_awesome_board.initBoard()
+        old_N = parameter.size
+        old_walls = parameter.walls
+        parameter.g_init = computeGraph()
+        human1 = Player_Human('Humain 1')
+        human2 = Player_Human('Humain 2')
+
+        play(human1, human2)
+        parameter.size = old_N
+        parameter.walls = old_walls
+        parameter.g_init = computeGraph()
+
+
+if __name__ == '__main__':
+    ui = QApplication(sys.argv)
+    main = Main()
+    sys.exit(ui.exec_())
